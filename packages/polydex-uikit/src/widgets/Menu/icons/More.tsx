5e6853b787831244a692db9ecb814be225ff4dcd
PolyDex <polydex.dev@gmail.com>
2021-12-06 14:57:35 +0700

    update menu icons

--- packages/polydex-uikit/src/widgets/Menu/icons/More.tsx
+++ packages/polydex-uikit/src/widgets/Menu/icons/More.tsx
@@ -5,10 +5,7 @@ import { SvgProps } from "../../../components/Svg/types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 24 24" {...props}>
-      <path
-        d="M12 4a8 8 0 108 8 8 8 0 00-8-8zm-4 9a1 1 0 111-1 1 1 0 01-1 1zm4 0a1 1 0 111-1 1 1 0 01-1 1zm4 0a1 1 0 111-1 1 1 0 01-1 1z"
-        fill="#425f7a"
-      />
+      <circle cx="12" cy="12" r="8" fill="#36c3ea" stroke="#0c2866" stroke-miterlimit="10" stroke-width="1.04"/><circle cx="12" cy="12" r="1.71" fill="#fff" stroke="#0c2866" stroke-miterlimit="10" stroke-width="0.5"/><circle cx="7.43" cy="12" r="1.71" fill="#fff" stroke="#0c2866" stroke-miterlimit="10" stroke-width="0.5"/><circle cx="16.57" cy="12" r="1.71" fill="#fff" stroke="#0c2866" stroke-miterlimit="10" stroke-width="0.5"/>
     </Svg>
   );
 };
